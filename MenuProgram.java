8ebb02c16b14843e05e3a50a655725cda65dd972
Christian Jarrell <christian.j.jarrell@gmail.com>
2026-05-10 15:29:38 -0400

    Add initial menu display

---
+++ MenuProgram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MenuProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1 - Enter two (2) numbers (x and y)");
+        System.out.println("2 - Print all numbers between x and y");
+        System.out.println("3 - FInd the average of all numbers between x and y");
+        System.out.println("4 - Find all prime numbers between x and y");
+        System.out.println("5 - Exit");
+    }
+}
